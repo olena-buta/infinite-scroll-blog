9fd6fb113643628a5b4f0fdcabd05b8394833e49
Olena Buta <olena.buta@gmail.com>
2020-07-29 12:51:09 +0300

    scroll down, show loader and fetch next set of posts, add filtering for fetched posts

--- js/script.js
+++ js/script.js
@@ -29,4 +29,46 @@ async function showPost() {
   });
 }
 
+function showLoading() {
+  loader.classList.add('show');
+
+  setTimeout(() => {
+    loader.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      showPost();
+    }, 300);
+  }, 1000);
+}
+
+function filterPosts() {
+  // const term = e.target.value.toUpperCase();
+  const term = filter.value.toUpperCase();
+
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach(post => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.includes(term) || body.includes(term)) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 showPost();
+
+// add scroll functionality
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
